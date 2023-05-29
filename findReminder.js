c604f06db7c756ef6e477d214ceb041f4e6a965c
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-05-28 23:22:01 -0700

    Create findReminder.js

---
+++ findReminder.js
@@ -0,0 +1,17 @@
+//Return the Remainder from Two Numbers
+
+const fnReminder = (vDividend, vDivisor) => {
+    return "The Reminder is "+ vDividend % vDivisor;
+}
+
+console.log(fnReminder(100, 10));//0
+console.log(fnReminder(450, 7));//2
+console.log(fnReminder(15, 4));//3
+
+/*
+Output
+----------
+The Reminder is 0
+The Reminder is 2
+The Reminder is 3
+*/
