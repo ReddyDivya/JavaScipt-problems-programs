f59c65b8fac83cb77f2227923c9cb8a15be724fc
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-05-28 23:04:21 -0700

    Create find largest number from an Array

---
+++ FindLargestNumInArray.js
@@ -0,0 +1,29 @@
+//Finding the largest number from an array
+
+/*
+  1. Initialize a variable "vLargestNum" to hold the maximum value and set it to the first element of the array.
+  2. Iterate through the remaining elements of the array starting from the second element.
+  3. For each element, compare it with the current maximum value (vLargestNum).
+  4. If the element is greater than "vLargestNum", update the value of "vLargestNum" to the current element.
+  5. Continue iterating through the rest of the array, updating "vLargestNum" whenever a larger element is found.
+  6. Once the iteration is complete, the variable "vLargestNum" will contain the largest number in the array.
+  Return the value of max.  
+*/
+
+function findLargestNum(vArray)
+{
+  let vLargestNum = vArray[0]; 
+
+  for(let i=1; i < vArray.length; i++){
+
+    if(vArray[i] > vLargestNum) 
+    {
+      vLargestNum = vArray[i];
+    }
+  }
+  
+  return vLargestNum;
+}//findLargestNum
+
+console.log(findLargestNum([7,88,99,2,1,4,66,100]))
+console.log(findLargestNum([227,288,39,332,441,34,616,0]))
