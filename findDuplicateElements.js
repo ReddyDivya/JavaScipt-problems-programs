cb2a475c498946eff131190ee4245e9e3d8d1ca8
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-05-30 02:46:14 -0700

    Update findDuplicateElements.js

--- findDuplicateElements.js
+++ findDuplicateElements.js
@@ -1,6 +1,9 @@
 //Find remove element from an array
 
 /*
+    filter - The filter() method creates a new array by filtering out the elements that do not meet the specified condition. 
+    In this case, the condition checks if the index of the current value is the same as the first occurrence of that value in the array, effectively removing duplicates.
+    ------------------------------------------------------------------------------------------------------------------------
     Syntax
     ------------
     array.filter(function(currentValue, index, arr), thisValue)
@@ -10,6 +13,7 @@
     index	- Optional. The index of the current element.
     arr	- Optional. The array of the current element.
     thisValue- Optional. Default undefined. A value passed to the function as its this value.
+    -----------------------------------------------------------------------------------------
 */    
 const array = [1, 22, 33, 3, 44, 4, 55, 7, 55, 44, 33, 22, 1];
 const uniqueArray = array.filter((currentValue, index, arr) => arr.indexOf(currentValue) === index);
