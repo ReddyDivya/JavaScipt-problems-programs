a7bd20da0279965847bb2d86bbf654b72a9af075
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-05-30 02:43:15 -0700

    Update findDuplicateElements.js

--- findDuplicateElements.js
+++ findDuplicateElements.js
@@ -1,14 +1,24 @@
-//Find duplicate element from an array
+//Find remove element from an array
 
-const findDuplicateElements = (vArray) => {
-    let vSameElement = vArray[0];
+/*
+    Syntax
+    ------------
+    array.filter(function(currentValue, index, arr), thisValue)
     
-    for(let i=1; i<vArray.length; i++){
-        if(vSameElement == vArray[i])
-            vSameElement = vArray[i];
-    }
-    
-    return vSameElement;
-}
-
-console.log("Duplicate Element from the Array is "+ findDuplicateElements([2,4,5,6,7,8,2,9]));//2
+    function() - Required. A function to run for each array element.
+    currentValue - Required. The value of the current element.
+    index	- Optional. The index of the current element.
+    arr	- Optional. The array of the current element.
+    thisValue- Optional. Default undefined. A value passed to the function as its this value.
+*/    
+const array = [1, 22, 33, 3, 44, 4, 55, 7, 55, 44, 33, 22, 1];
+const uniqueArray = array.filter((currentValue, index, arr) => arr.indexOf(currentValue) === index);
+console.log(uniqueArray);
+/*
+Output
+------------
+[
+   1, 22, 33, 3,
+  44,  4, 55, 7
+]
+*/
