6ad97b8a461525844446dede4817e2d4ae907967
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-07-02 23:35:53 -0700

    Create SumNumInArray.js

---
+++ SumNumInArray.js
@@ -0,0 +1,18 @@
+//sum of all the numbers in an array
+
+// Step 1: Create an array
+const numbers = [11, 32, 13, 94, 15];
+
+// Step 2: Initialize a variable to store the sum
+let sum = 0;
+
+// Step 3: Iterate through the array using a loop (e.g., for loop)
+for (let i = 0; i < numbers.length; i++) {
+  // Step 4: Add each element to the sum
+  sum += numbers[i];
+}
+
+// Step 5: After the loop, the variable 'sum' will hold the sum of all the numbers in the array.
+
+// Step 6: Print the result (optional)
+console.log(sum); //165
