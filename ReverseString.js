b5f380f911b6ad42b08c012ff1c7b821ba234781
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-08-02 06:58:33 -0700

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -1,6 +1,6 @@
 //Reverse a string
 
-//Method 1: 
+//Method 1: Using loop
 const reverseString = (str) => {
   let reversedStr = '';
   for (let i = str.length - 1; i >= 0; i--) {
@@ -17,8 +17,8 @@ console.log(reverseString('javascript'));//tpircsavaj
 
 
 /*
-  Method 2: using built-in JavaScript methods - Eg (palindrome)
-  ----------------------------------------------------
+  Method 2: using built-in JavaScript methods - split, reverse, join
+  ------------------------------------------------------------------
   split() function splits the string into an array of characters, then 
     -> ['p', 'a', 'l', 'i', 'n', 'd', 'r', 'o', 'm', 'e']
   reverse() reverses the order of the elements in the array, and finally, 
@@ -35,3 +35,18 @@ console.log(reverseStringBuiltInMethod('palindrome'));//emordnilap
 console.log(reverseStringBuiltInMethod('reverse'));//esrever
 console.log(reverseStringBuiltInMethod('programming'));//gnimmargorp
 console.log(reverseStringBuiltInMethod('javascript'));//tpircsavaj
+
+
+/*
+Method 3: using reduce
+  ------------------------------------------------------------------
+*/
+
+const reverseStringReduceMethod = (str) => {
+  return str.split("").reduce((reversedStr, char) =>  char + reversedStr, "");
+}
+
+console.log(reverseStringReduceMethod('palindrome'));//emordnilap
+console.log(reverseStringReduceMethod('reverse'));//esrever
+console.log(reverseStringReduceMethod('programming'));//gnimmargorp
+console.log(reverseStringReduceMethod('javascript'));//tpircsavaj
