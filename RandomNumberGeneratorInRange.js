b5de4532b338efdf404ec5d9ceaee632ddf3d3bb
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-08-03 05:35:56 -0700

    Update RandomNumberGeneratorInRange.js

--- RandomNumberGeneratorInRange.js
+++ RandomNumberGeneratorInRange.js
@@ -2,19 +2,21 @@
 
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
 	// write your solution here
-  
-  //if start range is greater than end range, switch them
+	//1. if start range is greater than end range, switch them
 	if(rangeStart > rangeEnd)
 	{
+		//destructure assignment
 		[rangeStart, rangeEnd] = [rangeEnd, rangeStart];
 	}
-
+	
+	//2. find the difference between the range
 	let difference = Math.floor(rangeEnd - rangeStart);
-
+	
+	//3. find the random number starting from the given rangeStart
 	let randomNum = Math.floor(Math.random() * difference) + rangeStart;
 	
 	return randomNum;
-}
+}//randomNumberGeneratorInRange
 
 console.log(`My random number: ${randomNumberGeneratorInRange(10, 5)}`);
 console.log(`My random number: ${randomNumberGeneratorInRange(100, 150)}`);
