f5cef9826537b3e8b43638a7dffae535169c4a7f
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-08-03 05:23:12 -0700

    Update ReverseGivenInteger.js

--- ReverseGivenInteger.js
+++ ReverseGivenInteger.js
@@ -1,6 +1,34 @@
 //Write a program to reverse a given integer number
 
-//Method 1: using built-in methods - split, reverse, join
+//Method 1: 
+function reverseGivenInteger(num) {
+    // write your solution here
+    let result = '';
+
+    while(num > 0)
+    {
+        //extract the last digit
+        let digit = num % 10;
+
+        //append the digit to the reversed number
+        result = result * 10 + digit;
+        
+        //remove the last digit from the number
+        num = Math.floor(num / 10);
+    }
+    
+    if(num < 0)
+    {
+        result = `Given ${num} is less than 0`;
+    }
+    return result;
+}
+
+console.log(reverseGivenInteger(3456));//6543
+console.log(reverseGivenInteger(-6));//Given -6 is less than 0
+console.log(reverseGivenInteger(900));//9
+
+//Method 2: using built-in methods - split, reverse, join
 function reverseGivenInteger(num) {
     let result = '';
     if(num > 0)
