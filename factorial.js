1267986cb32e5e1fdbc7267d2bf5e28cc9eedfa1
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2024-01-04 01:45:08 -0800

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -2,9 +2,23 @@
 /*
 factorial(5);//120
 factorial(4);//24
+
+Method 1:
 */
 const factorial = (n) => {
     return n > 0 ? n*factorial(n-1) : 1;
+}
+
+//Method 2:
+const factorial = (n) => {
+    let result = 1;
+    
+    for(let i=1; i<=n; i++)
+    {
+        result *= i;
+    }
+    
+    return result;
 }
 
 console.log(factorial(5));//120
