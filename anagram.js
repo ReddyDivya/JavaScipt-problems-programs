2ed71ddde6ec43a241fab9a2d988e8d94be446dd
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2024-01-04 04:05:23 -0800

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -2,9 +2,41 @@
 /*
 anagram("heart", "earth");//true
 anagram("heart!", " earth");//true
-anagram("lol", "loki");//true
+anagram("lol", "loki");//false
 */
-
-const anagram = () => {
+const anagram = (str1, str2) => {
+    let strCharCountObj1 = {};
+    let strCharCountObj2 = {};
     
+    str1 = str1.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    str2 = str2.replace(/[^a-zA-Z]/g, '').toLowerCase();        
+    
+    if(str1.length != str2.length)
+        return false;
+    
+    for(let char of str1){
+        strCharCountObj1[char] = strCharCountObj1[char] + 1 || 1;
+    }
+    
+    for(let char of str2)
+    {
+        strCharCountObj2[char] = strCharCountObj2[char] + 1 || 1;
+    }
+    
+    for(let key of str1)
+    {
+        if(strCharCountObj1[key] !== strCharCountObj2[key])
+        {
+            return false;
+        }
+    }
+    
+    return true;
 }
+
+console.log(anagram("heart!", " earth"));//true
+console.log(anagram("heart!! ! ", " earth "));//true
+console.log(anagram("listen", "silent"));//true
+console.log(anagram("hello", "world"));//false
+console.log(anagram("heart", "earth"));//true
+console.log(anagram("lol", "loki"));//false
